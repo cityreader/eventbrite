3db2c73903ca1dd805217b74383fb4891d43a787
Chao Chen <eric.chenchao@gmail.com>
2013-03-22 17:05:49 +1100

    Renovate the payment setting form

--- eventbrite.payment.js
+++ eventbrite.payment.js
@@ -1,21 +1,27 @@
-Drupal.behaviors.eventbritePayment = function (context) {
-  $('input:checkbox.eventbrite-payment-settings').click(function() {
-    if ($(this).attr('checked')) {
-      $(this).parent('label').parent('div.form-item').siblings('div.form-item').show();
-    }
-    else {
-      $(this).parent('label').parent('div.form-item').siblings('div.form-item').hide();
-      
-    }
-  });
+(function($) {
 
-  $('input:checkbox.eventbrite-payment-settings').each(function() {
-    if ($(this).attr('checked')) {
-      $(this).parent('label').parent('div.form-item').siblings('div.form-item').show();
-    }
-    else {
-      $(this).parent('label').parent('div.form-item').siblings('div.form-item').hide();
-    }
-  });
+Drupal.behaviors.eventbritePayment = {
+  attach: function(context, settings) {
+    console.log($('input:checkbox.eventbrite-payment-settings').length);
+    $('input:checkbox.eventbrite-payment-settings').click(function() {
+      if ($(this).attr('checked')) {
+        $(this).parent('div.form-item').siblings('div.form-item').show();
+      }
+      else {
+        $(this).parent('div.form-item').siblings('div.form-item').hide();
+        
+      }
+    });
 
-}
+    $('input:checkbox.eventbrite-payment-settings').each(function() {
+      if ($(this).attr('checked')) {
+        $(this).parent('div.form-item').siblings('div.form-item').show();
+      }
+      else {
+        $(this).parent('div.form-item').siblings('div.form-item').hide();
+      }
+    });
+  }
+};
+
+})(jQuery);
