8e8dd201b319adb591921677c4ab44f4bf3bf9a5
Chao Chen <eric.chenchao@gmail.com>
2013-05-09 17:23:30 +1000

    Change event edit link

--- themes/eventbrite-event.tpl.php
+++ themes/eventbrite-event.tpl.php
@@ -80,15 +80,15 @@
  * @ingroup themeable
  */
 ?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+<div id="node-<?php //print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php print $user_picture; ?>
+  <?php //print $user_picture; ?>
 
-  <?php print render($title_prefix); ?>
+  <?php //print render($title_prefix); ?>
   <?php if (!$page): ?>
     <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
   <?php endif; ?>
-  <?php print render($title_suffix); ?>
+  <?php //print render($title_suffix); ?>
 
   <?php if ($display_submitted): ?>
     <div class="submitted">
@@ -99,8 +99,10 @@
   <div class="content"<?php print $content_attributes; ?>>
     <?php
       // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
+      // hide($content['comments']);
+      // hide($content['links']);
+      dpm($content);
+      dpm(render($content));
       print render($content);
     ?>
   </div>
