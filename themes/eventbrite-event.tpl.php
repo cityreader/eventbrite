e13db957fc409c9415fb1ae7083744899e66eb69
Chao Chen <eric.chenchao@gmail.com>
2013-05-28 17:23:09 +1000

    Add wrapper for event

--- themes/eventbrite-event.tpl.php
+++ themes/eventbrite-event.tpl.php
@@ -80,23 +80,7 @@
  * @ingroup themeable
  */
 ?>
-<div id="node-<?php //print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
-  <?php //print $user_picture; ?>
-
-  <?php //print render($title_prefix); ?>
-  <?php if (!$page): ?>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-  <?php endif; ?>
-  <?php //print render($title_suffix); ?>
-
-  <?php if ($display_submitted): ?>
-    <div class="submitted">
-      <?php print $submitted; ?>
-    </div>
-  <?php endif; ?>
-
-  <div class="content"<?php print $content_attributes; ?>>
+  <div class="content">>
     <?php
       // We hide the comments and links now so that we can render them later.
       // hide($content['comments']);
@@ -107,8 +91,4 @@
     ?>
   </div>
 
-  <?php print render($content['links']); ?>
 
-  <?php print render($content['comments']); ?>
-
-</div>
