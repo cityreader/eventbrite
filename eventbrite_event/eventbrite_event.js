a1339d1ebbeb846d43266cf8b15cf81b66f87bb5
Chao Chen <eric.chenchao@gmail.com>
2013-05-29 17:44:53 +1000

    Add venue field for event page

--- eventbrite_event/eventbrite_event.js
+++ eventbrite_event/eventbrite_event.js
@@ -2,7 +2,10 @@
 
 Drupal.behaviors.eventbriteEvent = {
   attach: function (context, settings) {
-    
+    $('iframe').each(function () {
+        var iframe_height = $(this).height() + 70;
+        $(this).height(iframe_height);
+    });
   }
 }
   
---
+++ eventbrite_event/themes/eventbrite-event-venue.tpl.php
@@ -0,0 +1,3 @@
+<div class="event-venue">
+  <?php print $address; ?> | <?php print $name; ?>
+</div>
