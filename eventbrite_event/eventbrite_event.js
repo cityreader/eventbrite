f907ee6e668a9539d02a9b902bbfd92e9a81140c
Chao Chen <eric.chenchao@gmail.com>
2013-06-01 21:52:30 +1000

    Add privacy and status meta support for views

--- eventbrite_event/eventbrite_event.js
+++ eventbrite_event/eventbrite_event.js
@@ -3,7 +3,12 @@
 Drupal.behaviors.eventbriteEvent = {
   attach: function (context, settings) {
     $('iframe').each(function () {
-        var iframe_height = $(this).height() + 70;
+        var iframe_height = $(this).height();
+
+        if (iframe_height > 81 ) {
+          iframe_height += 70;
+        }
+        console.log(iframe_height);
         $(this).height(iframe_height);
     });
   }
